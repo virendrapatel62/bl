527167906f010c526179ba1adc1095618d1bc7f6
Virendra <patelvirendra62@gmail.com>
2019-08-23 17:28:01 +0530

    correction

--- models/materialModels/construction-material.js
+++ models/materialModels/construction-material.js
@@ -12,12 +12,12 @@ const constructionMaterialSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    brands: {
-        type: [mongoose.Schema.Types.ObjectId],
+    brands: [{
+        type: mongoose.Schema.Types.ObjectId,
         ref: 'brand',
         require: true,
         default: []
-    },
+    }],
     size: {
         type: [String],
         required: false,
@@ -40,23 +40,73 @@ const constructionMaterialSchema = new mongoose.Schema({
         ref: 'productCoreCategory',
         require: false
     },
-    subcategory: {
-        type: [mongoose.Schema.Types.ObjectId],
+    subcategory: [{
+        type: mongoose.Schema.Types.ObjectId,
         ref: 'productType',
         required: false,
         default: []
-    }
+    }]
 })
 
 //Model
 const constructionMaterial = mongoose.model('constructionMaterial', constructionMaterialSchema);
 
 // get all product types
-
 constructionMaterial.getAll = function () {
     // returning a promish
     return new Promise(async (resolve, reject) => {
-        const result = await constructionMaterial.find().select();
+        const result = await constructionMaterial.find().deepPopulate('subcategory brands').select();
+        resolve(result);
+    })
+}
+
+
+// get product By Id
+constructionMaterial.getById = function (id) {
+    // returning a promish
+    return new Promise(async (resolve, reject) => {
+        const result = await constructionMaterial.findById(id).deepPopulate('subcategory brands');
+        resolve(result);
+    })
+}
+// get all Name  ( Abstract name)
+constructionMaterial.getByName = function (name) {
+    // returning a promish
+    return new Promise(async (resolve, reject) => {
+        const result = await constructionMaterial.find({
+            name: {
+                $regex: new RegExp(name, 'i')
+            }
+        })
+        resolve(result);
+    })
+}
+// get by Size  
+constructionMaterial.getBySize = function (size) {
+    // returning a promish
+    return new Promise(async (resolve, reject) => {
+        const result = await constructionMaterial.find({
+            size: {
+                $in: size
+            }
+        }).deepPopulate('subcategory brands').select();
+        resolve(result);
+    })
+}
+
+// get by SubCategory  
+constructionMaterial.getBySubCategory = function (subCat) {
+    // returning a promish
+    return new Promise(async (resolve, reject) => {
+        const result = await constructionMaterial
+            .findOne()
+            .where(
+                {
+                    subcategory: subCat
+                }
+            )
+            .deepPopulate('subcategory brands').select()
+
         resolve(result);
     })
 }
@@ -64,10 +114,12 @@ constructionMaterial.getAll = function () {
 
 
 
+
 var deepPopulate = require('mongoose-deep-populate')(mongoose);
 constructionMaterialSchema.plugin(deepPopulate, {
     whitelist: [
-        '[subcategory]'
+        'subcategory',
+        'brands'
     ]
 });
 
--- routers/api/materials/construction-material.js
+++ routers/api/materials/construction-material.js
@@ -2,37 +2,35 @@ const Router = require('express').Router()
 const { constructionMaterial } = require('./../../../models/materialModels/construction-material')
 
 Router.get('/',async (req,res) =>{
-   const result = await constructionMaterial.find().deeppopulate('subcategory').select()
+    const result = await constructionMaterial.getAll();
     res.send(result)
 });
 
-Router.get('/:id',async (req,res) =>{
-    const result = await constructionMaterial.findById(req.params.id).populate('subcategory')
+Router.get('/id/:id',async (req,res) =>{
+    var id = req.params.id;
+    const result = await constructionMaterial.getById(id);
     res.send(result)
 });
 
 // detail of material by providing full name or abstract name
 Router.get('/name/:name',async (req,res) =>{
     const name = req.params.name
-    const result = await constructionMaterial.find({
-                        name:{
-                            $regex: new RegExp(name,'i')
-                        }
-                    })
+    const result = await constructionMaterial.getByName(name);
     res.send(result)
 });
 
 
 // size pass retuning brands
 Router.get('/size/:size',async (req,res) =>{
-    console.log(req.params.size)
-    const result = await constructionMaterial.find().where({size:req.params.size}).select('brands')
+    let size = req.params.size
+    const result = await constructionMaterial.getBySize(size);
+    console.log(result);
     res.send(result)
 });
 
 // sub category  the nproduct 
 Router.get('/subcategory/:subcategoryid',async (req,res) =>{
-   const result = await constructionMaterial.findOne().where({subcategory:req.params.subcategoryid}).select()
+    const result = await constructionMaterial.getBySubCategory(req.params.subcategoryid);
     res.send(result)
 });
 
