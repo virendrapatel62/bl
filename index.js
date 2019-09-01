0623b052a29cabd583cf628eb6dec1133dd5b868
Virendra <patelvirendra62@gmail.com>
2019-09-01 08:15:49 +0530

    updated

--- index.js
+++ index.js
@@ -6,16 +6,35 @@ const logger = require('debug')('index')
 const winston = require('winston')
 const config = require('config')
 
+// for printing the request info
+app.use(function (req, res, next) {
+
+    if(req.url.includes('css') 
+    || req.url.includes('js') 
+    || req.url.includes('customcss') 
+    || req.url.includes('customjs') 
+    || req.url.includes('js') 
+    || req.url.includes('png') 
+    || req.url.includes('jpg')){
+
+    }else{
+
+        console.log(`Url : ${req.url} , Body : ${req.method}` );
+    }
+    next()
+    // return;
+})
+
 
 // for uncaughtException 
-process.on('uncaughtException' , (ex)=>{
+process.on('uncaughtException', (ex) => {
     console.log(ex);
-    
-    winston.error(ex.message , ex);
+
+    winston.error(ex.message, ex);
 })
 
 // logging setup 
-winston.add(new winston.transports.File({filename : 'errorLog.log'}));
+winston.add(new winston.transports.File({ filename: 'errorLog.log' }));
 
 require('express-async-errors')
 
@@ -27,7 +46,6 @@ require('./startup/middleware')(app);
 
 // for All routes..
 require('./startup/routes')(app)
-require('./startup/material-routes')(app)
 require('./startup/APIroutes')(app)
 
 // production middldewares
@@ -41,8 +59,8 @@ app.use(require('./middlewares/error'));
 
 
 // configuration
-console.log(`email password :`  + config.get('email_password'));
-console.log(`email :`  + config.get('email'));
+console.log(`email password :` + config.get('email_password'));
+console.log(`email :` + config.get('email'));
 
 
 
@@ -51,26 +69,24 @@ const PORT = process.env.PORT || 80;
 console.log(PORT);
 
 var reload = require('reload')
-reload(app).then(function (reloadReturned) {
+let server = null;
 
-    app.listen(PORT, () => {
-       logger(`Listening at port ${PORT}.....`);
-        
-       console.log(`Listening at port ${PORT}.....`);
-        if (process.send) {
-            process.send('online');
-        }
-    })// reloadReturned is documented in the returns API in the README
-
+server = app.listen(PORT, () => {
+    logger(`Listening at port ${PORT}.....`);
 })
+// reload(app).then(function (reloadReturned) {
+//     server = app.listen(PORT, () => {
+//        logger(`Listening at port ${PORT}.....`);
+
+//        console.log(`Listening at port ${PORT}.....`);
+//         if (process.send) {
+//             process.send('online');
+//         }
+//     })// reloadReturned is documented in the returns API in the README
+
+// })
 
 
 
-// async function test(){
-//     const { User } = require('./models/user')
-//     const result = await User.find({_id : '5d1f2d7985a99c7f8c8fa20d'}).select();
-//     console.log(result);
-    
-// }
 
-// test()
+module.exports = server
